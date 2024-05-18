ed5800641d5f8d2c2d661ad8def23a30672c1755
Trickster2001 <keyurmurkar@gmail.com>
2024-05-18 23:29:21 +0530

    todoSchema

---
+++ models/todos/todo.models.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const todoSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    isComplete: {
+      type: Boolean,
+      default: false
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    },
+    subTodo: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "SubTodo"
+      }
+    ] // array or subtodos
+  }, {timestamps: true}
+)
+
+export const Todo = mongoose.model("Todo", todoSchema);
